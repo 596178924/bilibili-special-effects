213b01d3ddccfa24f83e6bf18a1f957543b6cfe6
metaDragonMaster <2327139286@qq.com>
2022-02-12 16:54:39 +0800

    'FollowMouse'

---
+++ FollowMouse/.history/js/index_20220212164927.js
@@ -0,0 +1,45 @@
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let points = [];
+let live = 50;
+let colors = [
+    "236,204,104",
+    "255,71,87",
+    "112,161,255",
+    "123,237,159"
+]
+window.addEventListener('mousemove', function (el) {
+    for (let i = 0; i < 15; i++) {
+        points.push({
+            sx: el.x,
+            sy: el.y,
+            vx: 0.5 - Math.random(),
+            vy: 0.5 - Math.random(),
+            life: live,
+            color: colors[parseInt(Math.random() * colors.length)],
+            size: Math.random() * 5
+        })
+    }
+})
+
+function drawpoints() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    let point;
+    for (let i = 0; i < points.length; i++) {
+        point = points[i];
+        ctx.beginPath();
+        ctx.arc(point.sx, point.sy, point.size, Math.PI * 2, false);
+        ctx.fillStyle = `rgba(${point.color},${point.life / live})`;
+        ctx.fill();
+        point.life--;
+        if (point.life <= 0) {
+            points.splice(i, 1)
+        }
+        point.sx += point.vx * 3;
+        point.sy += point.vy * 3;
+    }
+}
+requestAnimationFrame(drawpoints)
+// setInterval(drawpoints, 10)
